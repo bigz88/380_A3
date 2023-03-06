8f34e84baad1800eb5508b05e37a2f0716fece6a
bigz88 <zaid.ahmed@ucalgary.ca>
2023-03-05 21:37:56 -0700

    More functs to RobotDataLine (still have some errors)

--- src/RobotDataLine.java
+++ src/RobotDataLine.java
@@ -14,4 +14,32 @@ public class RobotDataLine {
     private static final Pattern DATE_PATTERN = Pattern.compile(DATE_REGEX);
     private static final String ROBOT_REGEX = "\\s([0-9]{3}[A-Z]{1})\\s";
     private static final Pattern ROBOT_PATTERN = Pattern.compile(ROBOT_REGEX);
+
+    public RobotDataLine(String line){
+
+    }
+
+    public String getRobotID(){
+        return this.robotID;
+    }
+
+    public String getDataLine(){
+        return this.dataLine;
+    }
+
+    public Sensor getSensor() {
+        return this.sensor;
+    }
+
+    public Movement getMovement() {
+        return movement;
+    }
+
+    public LocalDate getDate(){
+        return this.date;
+    }
+
+    public Object clone() throws CloneNotSupportedException{
+        return super.clone();
+    }
 }
